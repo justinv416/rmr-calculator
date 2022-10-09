0cc00df5f63142fd0f4f364ef61893dbff75a9e3
justinv416 <vien_justin@hotmail.com>
2022-10-09 17:57:46 -0400

    styling and conditional rendering fixes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Form from './components/Form';
+import { motion } from 'framer-motion';
 import { useState } from 'react';
 
 
@@ -14,6 +15,7 @@ function App() {
   const [showGender, setShowGender] = useState(false);
   const [maleGender, setMaleGender] = useState(false);
   const [femaleGender, setFemaleGender] = useState(false);
+  const [showRmr, setShowRmr] = useState(false);
 
   const handleWeightInput = (e) => {
     setInputWeight(parseInt(e.target.value));
@@ -34,9 +36,8 @@ function App() {
     } else if (femaleGender) {
       setFemaleRmr((10 * inputWeight) + (6.25 * inputHeight) - (5 * inputAge) - 161) 
     }
+    setShowRmr(true);
   }
-
-  const handleShowGender = () => setShowGender(true); 
 
   const handleShowformMale = () => {
     setMaleGender(true);
@@ -58,24 +59,27 @@ function App() {
   return (
     <div className="App">
       <div className="wrapper">
-        <h1>RMR Calculator</h1>
-        <h2>Trying to lose weight?</h2>
-        {showStart && <button onClick={handleShowStart}>Get Started</button>}
-        {showGender && <div className="">
-          <h3>Are you...</h3>
-          <button onClick={handleShowformMale}>Male</button>
-          <button onClick={handleShowformFemale}>Female</button>
-        </div>}
-        {showForm && <Form 
-          handleWeightInput={handleWeightInput} 
-          handleHeightInput={handleHeightInput} 
-          handleAgeInput={handleAgeInput} 
-          handleSubmit={handleSubmit} 
-        />}
-        <p>{inputWeight}</p>
-        <p>{inputHeight}</p>
-        <p>{inputAge}</p>
-        {maleGender ? <p>RMR:{maleRmr}</p> : <p>RMR:{femaleRmr}</p>}
+        <div className="content-container">
+          <motion.h1 animate={{y: -20, opacity: 1}} initial={true}>Low-Cal</motion.h1>
+          {showStart && <h2>Trying to lose weight?</h2>}
+          {showStart && <button className="button" onClick={handleShowStart}>Get Started</button>}
+          {showGender && 
+            <div>
+              <h3>Are you...</h3>
+              <button className="button" onClick={handleShowformMale}>Male</button>
+              <button className="button" onClick={handleShowformFemale}>Female</button>
+            </div>
+          }
+          {showForm && <h2>Fill in the form</h2>}
+          {showForm && <Form 
+            handleWeightInput={handleWeightInput} 
+            handleHeightInput={handleHeightInput} 
+            handleAgeInput={handleAgeInput} 
+            handleSubmit={handleSubmit} 
+          />}
+          {showRmr && <p>Your resting metabolic rate is:</p>}
+          {maleGender ? <p>{maleRmr}</p> : <p>{femaleRmr}</p>}
+        </div>
       </div>
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -2,15 +2,33 @@ import React from 'react'
 
 const Form = (props) => {
   return (
-    <div>
-        <form action="" onSubmit={props.handleSubmit}>
+    <div className='form-container'>
+        <form className='input-form' action="" onSubmit={props.handleSubmit}>
             <label htmlFor="wight-input">Weight(KG):</label>
-            <input id='weight-input' type="text" value={props.inputWeight} onChange={props.handleWeightInput} />
+            <input 
+              className="input" 
+              id='weight-input' 
+              type="text" 
+              value={props.inputWeight} 
+              onChange={props.handleWeightInput} 
+            />
             <label htmlFor="height-input">Height(CM):</label>
-            <input id='height-input' type="text" value={props.inputHeight} onChange={props.handleHeightInput} />
+            <input 
+              className="input" 
+              id='height-input' 
+              type="text" 
+              value={props.inputHeight} 
+              onChange={props.handleHeightInput} 
+            />
             <label htmlFor="age-input">Age(in Years):</label>
-            <input id='age-input' type="text" value={props.inputAge} onChange={props.handleAgeInput} />
-            <button type='submit'>Submit</button>
+            <input 
+              className="input" 
+              id='age-input' 
+              type="text" 
+              value={props.inputAge} 
+              onChange={props.handleAgeInput} 
+            />
+            <button className="button" type='submit'>Submit</button>
         </form>
     </div>
   )
